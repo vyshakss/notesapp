ee46acca49618a28537fb0abee116f9d5a428f1e
Vyshak S <vyshaks856@gmail.com>
2026-01-15 16:47:16 +0530

    Initial commit

---
+++ notes.sh
@@ -0,0 +1,10 @@
+notes_file="/mnt/163A2B503A2B2C65/notepadapp/test1.txt"
+mkdir -p "$(dirname "$notes_file")"
+if [ ! -f "$notes_file" ]; then
+touch "$notes_file"
+fi
+content=$(zenity --text-info --editable --filename="$notes_file" --title="Notes" --ok-label="Save" --cancel-label="Discard" --width=600 --height=600 --font="Monospace 12")
+if [ $? -eq 0 ]; then 
+echo "$content" > "$notes_file"
+zenity --notification --text="Saved"
+fi
