f1b682f02806bf86fb111d72964392737bba5f10
Vyshak S <vyshaks856@gmail.com>
2026-01-18 19:51:26 +0530

    added some features

--- notes.sh
+++ notes.sh
@@ -5,9 +5,18 @@ while true; do
 if [ ! -f "$current_file" ]; then
 touch "$current_file"
 fi
-content=$(zenity --text-info --editable --filename="$current_file" --title="Notes" --ok-label="Save" --cancel-label="Discard" --extra-button="Open" --width=600 --height=600 --font="Monospace 12")
+content=$(zenity --text-info --editable --filename="$current_file" --title="Notes" --ok-label="Save" --cancel-label="Discard" --extra-button="Open" --extra-button="New file" --width=600 --height=600 --font="Monospace 12")
 exit_code=$?
-if [ "$content" = "Open" ]; then
+if [ "$content" = "New file" ]; then
+new_path=$(zenity --file-selection --save \
+                   --title="Create New Note" \
+                   --filename="/mnt/163A2B503A2B2C65/notepadapp/Untitled.txt")
+if [ -n "$new_path" ]; then
+touch "$new_path"
+current_fie="$new_path"
+fi
+continue
+elif [ "$content" = "Open" ]; then
 new_selection=$(zenity --file-selection --filename="/mnt/163A2B503A2B2C65/notepadapp/")
 if [ -n "$new_selection" ]; then
 current_file="$new_selection"
