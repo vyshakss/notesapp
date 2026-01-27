993990403c2521ac723929b585847568e415c29b
Vyshak S <vyshaks856@gmail.com>
2026-01-27 23:19:16 +0530

    bug fixes

--- notes.sh
+++ notes.sh
@@ -2,18 +2,19 @@ current_file="/mnt/163A2B503A2B2C65/notepadapp/test1.txt"
 mkdir -p "$(dirname "$current_file")"
 while true; do
 #to create file if its not there
-if [ ! -f "$current_file" ]; then
-touch "$current_file"
+if [  -f "$current_file" ]; then
+load_arg="--filename=$current_file"
+else
+load_arg=""
 fi
-content=$(zenity --text-info --editable --filename="$current_file" --title="Notes" --ok-label="Save" --cancel-label="Discard" --extra-button="Open" --extra-button="New file" --width=600 --height=600 --font="Monospace 12")
+content=$(zenity --text-info --editable $load_arg --title="$current_file" --ok-label="Save" --cancel-label="Discard" --extra-button="Open" --extra-button="New file" --width=600 --height=600 --font="Monospace 12")
 exit_code=$?
 if [ "$content" = "New file" ]; then
 new_path=$(zenity --file-selection --save \
                    --title="Create New Note" \
                    --filename="/mnt/163A2B503A2B2C65/notepadapp/Untitled.txt")
 if [ -n "$new_path" ]; then
-touch "$new_path"
-current_fie="$new_path"
+current_file="$new_path"
 fi
 continue
 elif [ "$content" = "Open" ]; then
